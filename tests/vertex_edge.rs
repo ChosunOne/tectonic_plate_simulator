1f9f6271309e204d1ee919cfa67e98ecc4888395
ChosunOne <chosunone@protonmail.com>
2025-12-22 21:37:50 +0900

    test: add test for vertex velocity calculation

---
+++ tests/vertex_edge.rs
@@ -0,0 +1,28 @@
+use bevy::math::Vec3;
+use tectonic_plate_simulator::resources::mantle_grid::MantleGrid;
+
+#[test]
+fn vertex_edge_adjacency_exists() {
+    let grid = MantleGrid::new(20);
+    let vertex_edge = grid.vertex_edge_adjacency();
+    let num_vertices = grid.sphere().raw_points().len();
+
+    assert!(!vertex_edge.is_empty());
+
+    let mut degree_5_count = 0;
+    let mut degree_6_count = 0;
+
+    for v in 0..vertex_edge.len() {
+        let degree = vertex_edge.count(v);
+        if degree == 5 {
+            degree_5_count += 1;
+        } else if degree == 6 {
+            degree_6_count += 1;
+        } else {
+            panic!("Unexpected vertex degree: {degree}");
+        }
+    }
+
+    assert_eq!(degree_5_count, 12);
+    assert_eq!(degree_6_count, num_vertices - 12);
+}
