ecbfad818296b245b416167a82f76ed6abb605c6
ChosunOne <chosunone@protonmail.com>
2026-01-08 22:51:26 +0900

    feat: tweak parameters

--- src/plugins/mantle_grid.rs
+++ src/plugins/mantle_grid.rs
@@ -19,7 +19,7 @@ pub struct MantleGridPlugin;
 
 impl Plugin for MantleGridPlugin {
     fn build(&self, app: &mut App) {
-        let grid = MantleGrid::new(20);
+        let grid = MantleGrid::new(100);
         app.insert_resource(grid.clone());
         let render_app = app.sub_app_mut(RenderApp);
         render_app.insert_resource(grid);
